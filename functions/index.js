d792461082372b1507edec68ac5ee4b8249c80a6
boolfalse <boolfalse@mail.ru>
2020-05-16 02:49:46 +0400

    getScrems function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
+
 const functions = require('firebase-functions');
+const admin = require('firebase-admin'); // admin SDK for firebase database
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// already knows about the project (it means the database) from .firebaserc file,
+// so we don't need to pass an argument in the initializeApp function
+admin.initializeApp();
 
+// https://us-central1-socialapp-3aeeb.cloudfunctions.net/helloWorld
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello World!");
+});
+
+// https://us-central1-socialapp-3aeeb.cloudfunctions.net/getScreams
+exports.getScreams = functions.https.onRequest((req, res) => {
+    // https://firebase.google.com/docs/firestore/query-data/get-data#get_all_documents_in_a_collection
+    admin.firestore()
+        .collection('screams')
+        .get()
+        .then(snapshotData => {
+            let screams = [];
+            snapshotData.forEach(doc => {
+                screams.push(doc.data());
+            });
+            return res.json(screams);
+        })
+        .catch(err => {
+            console.log('Error getting documents', err);
+        });
 });
