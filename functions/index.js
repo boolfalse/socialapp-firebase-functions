16a29f00168401ab8ff45ce96c333f0ab72e6d2d
boolfalse <boolfalse@mail.ru>
2020-05-25 19:03:01 +0400

    working on Auth (pending)

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,8 @@ const env = require('dotenv').config();
 const functions = require('firebase-functions');
 const serviceAccount = require('./firebase-adminsdk-sa-pk.json');
 const admin = require('firebase-admin'); // admin SDK for firebase database
-const express = require('express');
-const app = express();
+const db = admin.firestore();
+const app = require('express')();
 
 // generate config file from here
 // Firebase Console > Settings > Project Settings > Service Accounts
@@ -14,10 +14,23 @@ admin.initializeApp({
     databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
 });
 
+const firebase = require('firebase');
+const firebaseConfig = {
+    apiKey: process.FIREBASE_WEB_APP_API_KEY,
+    authDomain: process.FIREBASE_WEB_APP_AUTH_DOMAIN,
+    databaseURL: process.FIREBASE_WEB_APP_DATABASE_URL,
+    projectId: process.FIREBASE_WEB_APP_PROJECT_ID,
+    storageBucket: process.FIREBASE_WEB_APP_STORAGE_BUCKET,
+    messagingSenderId: process.FIREBASE_WEB_APP_MESSAGING_SENDER_ID,
+    appId: process.FIREBASE_WEB_APP_APP_ID,
+    measurementId: process.FIREBASE_WEB_APP_MEASUREMENT_ID,
+};
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
 // {{FB_FS_DB}}/getScreams
 app.get('/screams', (req, res) => {
-    admin.firestore()
-        .collection('screams')
+    db.collection('screams')
         .orderBy('createdAt', 'DESC')
         .get()
         .then(snapshotData => {
@@ -44,8 +57,7 @@ app.post('/screams', (req, res) => {
         body: req.body.body,
         createdAt: new Date().toISOString() // admin.firestore.Timestamp.fromDate(new Date())
     };
-    admin.firestore()
-        .collection('screams')
+    db.collection('screams')
         .add(scream)
         .then(doc => {
             return res.json({
@@ -56,6 +68,32 @@ app.post('/screams', (req, res) => {
             // console.log(err);
             return res.status(500).json({
                 error: `Something went wrong`
+            });
+        });
+});
+
+app.post('/sign-up', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    // TODO: validate request data
+
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return res.status(201).json({
+                message: `User ${data.user.uid} signed up successfully.`
+            });
+        })
+        .catch(err => {
+            // console.error(err);
+            return res.status(500).json({
+                error: err.code
             });
         });
 });
